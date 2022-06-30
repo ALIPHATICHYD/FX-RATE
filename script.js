9c65760125b1f01b73737a3dc186cc4fbda1410f
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2022-07-01 00:10:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const currencyE1_one = document.getElementById('currency-one');
 
 const currencyE1_two = document.getElementById('currency-two');
+
+const amountE1_one = document.getElementById('amount-one');
+
+const amountE1_two = document.getElementById('amount-two');
