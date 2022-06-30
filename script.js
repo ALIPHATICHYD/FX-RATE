ae1e47b7f2e511741fa5bb670ead22fe0aa35456
Adegoke Fuad Ajibola <105937740+ALIPHATICHYD@users.noreply.github.com>
2022-07-01 00:10:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ const currencyE1_two = document.getElementById('currency-two');
 const amountE1_one = document.getElementById('amount-one');
 
 const amountE1_two = document.getElementById('amount-two');
+
+const rateEl = document.getElementById('rate');
+
+const swap = document.getElementById('swap');
